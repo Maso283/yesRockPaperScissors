eb2d0b8a52b94e11d7b3e580cb2fab425e6357e0
Maso283 <justin.roberts.21221@protonmail.com>
2021-03-14 07:44:30 -0600

    Added in a few comments

--- yesRockPaperScissors.js
+++ yesRockPaperScissors.js
@@ -1,1 +1,16 @@
-console.log("Hello world!");
+//console.log("Hello world!");
+
+//"playerSelection"
+function playerSelection(select) {
+	select = prompt("Rock, paper, or scissors?") //Needs to be case insensitive
+}
+
+//"computerSelection"
+function computerSelection() {
+	//computerSelection = A random number picked out of a list of three
+}
+
+//"game function"
+function game(playerSelection, computerSelection) {
+
+}
